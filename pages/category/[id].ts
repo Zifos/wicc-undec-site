a33963a5126a5c2edd4e796c756c3cd840f694bb
jnfrati <nicofrati@gmail.com>
2021-03-15 20:27:26 -0300

    Initialized categories endpoints

---
+++ pages/category/[id].ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import connectDB from "../../utils/db_connection.handler";
+
+const getCategoryById = (_req: NextApiRequest, res: NextApiResponse): void => {
+  res.status(200).json({ name: "Pong" });
+};
+
+const removeCategoryById = (
+  _req: NextApiRequest,
+  res: NextApiResponse
+): void => {
+  res.status(200).json({ success: true });
+};
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req;
+  switch (method) {
+    case "GET":
+      getCategoryById(req, res);
+      break;
+    case "DELETE":
+      removeCategoryById(req, res);
+      break;
+    default:
+      res.status(404).json({ error: "Resource not found" });
+  }
+};
+
+export default connectDB(handler);
---
+++ pages/category/index.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import connectDB from "../../utils/db_connection.handler";
+
+const getAllCategories = (_req: NextApiRequest, res: NextApiResponse): void => {
+  res.status(200).json({ name: "Pong" });
+};
+
+const createCategory = (_req: NextApiRequest, res: NextApiResponse): void => {
+  res.status(200).json({ success: true });
+};
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req;
+  switch (method) {
+    case "GET":
+      getAllCategories(req, res);
+      break;
+    case "POST":
+      createCategory(req, res);
+      break;
+    default:
+      res.status(404).json({ error: "Resource not found" });
+  }
+};
+
+export default connectDB(handler);
