c217f147f75cff81e8392f62e4eff21b76f3ab1f
jnfrati <nicofrati@gmail.com>
2021-03-21 22:06:32 -0300

    created interfaces for posts

--- models/post.model.ts
+++ models/post.model.ts
@@ -2,6 +2,18 @@ import { Schema, model, models } from "mongoose";
 
 const { Types } = Schema;
 
+export interface IFile {
+  fileName: string;
+  fileLocation: string;
+}
+
+export interface IPost {
+  _id: string;
+  title: string;
+  audio: IFile;
+  pdf: IFile;
+}
+
 export const PostSchema = new Schema({
   pdf: { type: Object, default: {} },
   audio: { type: Object, default: {} },
@@ -9,6 +21,4 @@ export const PostSchema = new Schema({
   category: { type: Types.ObjectId, ref: "category" },
 });
 
-const PostModel = model("post", PostSchema);
-
-export default models.post || PostModel;
+export default models.post || model("post", PostSchema);
