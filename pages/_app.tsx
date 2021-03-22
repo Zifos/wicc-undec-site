77cf78a7a5fd83e32226d069547ae9dff2d896b2
francomd <francomdalmasso@gmail.com>
2021-03-21 23:54:00 -0300

    Add home and post route, player and PDF preview components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,20 +2,31 @@ import { AppProps } from "next/app";
 import "antd/dist/antd.less";
 import { ThemeProvider } from "styled-components";
 import { Layout } from "antd";
+import { useRouter } from "next/router";
 import { theme } from "../constants/theme";
 import Sider from "../components/Sider";
 import Footer from "../components/Footer";
 
-const MyApp = ({ Component, pageProps }: AppProps): React.ReactNode => (
-  <ThemeProvider theme={{ ...theme }}>
-    <Layout style={{ height: "100vh" }}>
-      <Sider />
-      <Layout>
-        <Component {...pageProps} />
-        <Footer />
-      </Layout>
-    </Layout>
-  </ThemeProvider>
-);
+const MyApp = ({ Component, pageProps }: AppProps): React.ReactNode => {
+  const currentRoute = useRouter().pathname;
+
+  return (
+    <ThemeProvider theme={{ ...theme }}>
+      {currentRoute === "/home" || currentRoute === "/post/[...]" ? (
+        <Layout style={{ height: "100vh", overflow: "hidden" }}>
+          <Component {...pageProps} />
+        </Layout>
+      ) : (
+        <Layout style={{ height: "100vh" }}>
+          <Sider />
+          <Layout>
+            <Component {...pageProps} />
+            <Footer />
+          </Layout>
+        </Layout>
+      )}
+    </ThemeProvider>
+  );
+};
 
 export default MyApp;
