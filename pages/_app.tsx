10cb76ff4c17234abf5aa094e76a35186e04edaf
jnfrati <nicofrati@gmail.com>
2021-03-30 19:29:36 -0300

    Added spiner between page changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,32 +2,35 @@ import { Provider } from "next-auth/client";
 import type { AppProps } from "next/app";
 import "antd/dist/antd.less";
 import { ThemeProvider } from "styled-components";
-import { Layout } from "antd";
+import { Layout, Spin } from "antd";
 import { useRouter } from "next/router";
 import { theme } from "../constants/theme";
 import Sider from "../components/Sider";
 import Footer from "../components/Footer";
+import useLoadingPage from "../hooks/useLoadingPage";
 import "isomorphic-unfetch";
 
 const MyApp = ({ Component, pageProps }: AppProps): React.ReactNode => {
   const currentRoute = useRouter().pathname;
-
+  const [loadingPage] = useLoadingPage();
   return (
     <Provider session={pageProps.session}>
       <ThemeProvider theme={{ ...theme }}>
-        {!currentRoute.includes("/admin") ? (
-          <Layout style={{ height: "100vh", overflow: "hidden" }}>
-            <Component {...pageProps} />
-          </Layout>
-        ) : (
-          <Layout style={{ height: "100vh" }}>
-            <Sider />
-            <Layout>
+        <Spin spinning={loadingPage}>
+          {!currentRoute.includes("/admin") ? (
+            <Layout style={{ height: "100vh", overflow: "hidden" }}>
               <Component {...pageProps} />
-              <Footer />
             </Layout>
-          </Layout>
-        )}
+          ) : (
+            <Layout style={{ height: "100vh" }}>
+              <Sider />
+              <Layout>
+                <Component {...pageProps} />
+                <Footer />
+              </Layout>
+            </Layout>
+          )}
+        </Spin>
       </ThemeProvider>
     </Provider>
   );
