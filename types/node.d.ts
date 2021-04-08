12dda7806838ce68cda4f3a3c45090211ddba7c2
jnfrati <nicofrati@gmail.com>
2021-04-07 22:42:30 -0300

    Implemented env variables for firebase

--- types/node.d.ts
+++ types/node.d.ts
@@ -8,10 +8,14 @@ declare namespace NodeJS {
     AUTH0_CLIENT_ID: string;
     AUTH0_CLIENT_SECRET: string;
     AUTH0_DOMAIN: string;
-    FIREBASE_PRIVATE_KEY: string;
-    FIREBASE_CLIENT_EMAIL: string;
+    FIREBASE_API_KEY: string;
+    FIREBASE_AUTH_DOMAIN: string;
+    FIREBASE_DB_URL: string;
     FIREBASE_PROJECT_ID: string;
-    FIREBASE_STORAGE_BUCKET_URL: string;
+    FIREBASE_STORAGE_BUCKET: string;
+    FIREBASE_MESSAGING_SENDER_ID: string;
+    FIREBASE_APP_ID: string;
+    FIREBASE_MEASSUREMENT_ID: string;
     REMARK_HOST: string;
     REMARK_SITE_ID: string;
     REMARK_LOCALE: string;
--- utils/firebase_connection.ts
+++ utils/firebase_connection.ts
@@ -1,28 +1,15 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB7L3fYfuluouySkfL8t9S8kgNr_lSZ6Dg",
-  authDomain: "tests-frati.firebaseapp.com",
-  databaseURL: "https://tests-frati.firebaseio.com",
-  projectId: "tests-frati",
-  storageBucket: "tests-frati.appspot.com",
-  messagingSenderId: "564964559853",
-  appId: "1:564964559853:web:85442f642e5685c7e935e4",
-  measurementId: "G-4XJ5470PKB",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DB_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASSUREMENT_ID,
 };
-
-// const certConfig = {
-//   privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-//   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-// };
-
-// if (!admin.apps.length) {
-//   admin.initializeApp({
-//     credential: admin.credential.cert(certConfig),
-//     storageBucket: process.env.FIREBASE_STORAGE_BUCKET_URL,
-//   });
-// }
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
