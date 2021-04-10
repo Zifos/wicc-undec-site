8d3a0d27a7218735f43d906aa07ae72de400a2e2
jnfrati <nicofrati@gmail.com>
2021-04-10 16:11:10 -0300

    Fixed firebase environment variables

--- next.config.js
+++ next.config.js
@@ -32,6 +32,16 @@ module.exports = withCSS({
         REMARK_HOST: process.env.REMARK_HOST,
         REMARK_SITE_ID: process.env.REMARK_SITE_ID,
         REMARK_LOCALE: process.env.REMARK_LOCALE,
+        firebaseConfig: {
+          apiKey: process.env.FIREBASE_API_KEY,
+          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          databaseURL: process.env.FIREBASE_DB_URL,
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+          appId: process.env.FIREBASE_APP_ID,
+          measurementId: process.env.FIREBASE_MEASSUREMENT_ID,
+        },
       },
       webpack: (config) => {
         // Note: we provide webpack above so you should not `require` it
